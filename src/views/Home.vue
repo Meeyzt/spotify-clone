c9baf972747be5a60280b84ccef0ea9d2f224426
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-09 13:48:43 +0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="home">
+<div>
+  <div class="wrapper">
+
+    <div class="flex flex-row">
+
+      <Sidebar />
+
+      <div class="flex flex-col">
+        <Navbar/>
+
+        <Content/>
+      </div>
+
+    </div>
 
   </div>
+  <Footer/>
+</div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
+import Sidebar from '../components/Sidebar.vue';
+import Footer from '../components/Footer.vue';
+import Content from '../components/Content.vue';
+import Navbar from '../components/Navbar.vue';
+
 export default {
   name: 'Home',
+
   components: {
+    Sidebar,
+    Footer,
+    Content,
+    Navbar,
+  },
+
+  computed: {
+    ...mapState([
+      'isAuthenticated',
+    ]),
   },
 };
 </script>
