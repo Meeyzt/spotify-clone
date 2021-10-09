eb3ba476c5fffdf34fc42bbbd9377bf5e06ad217
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-09 16:03:18 +0300

    Show playlists

--- src/components/sidebar/Playlists.vue
+++ src/components/sidebar/Playlists.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="flex-auto bg-pink-300 overflow-auto">
-      Playlist
+  <div class="mx-6 flex-auto border-t border-white border-opacity-20 overflow-auto">
+    <div
+     :v-for="index in 40" :v-key="index"
+     class="text-sm opacity-70 cursor-pointer flex pt-2
+      flex-row hover:rounded hover:opacity-100 gap-x-4 text-white">
+        new playlist #001
+    </div>
   </div>
 </template>
 
