2e20b14f2c473d97c827714b5e6d0df9582b1dcc
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-09 13:50:11 +0300

    Update Nav.vue

--- src/components/menuItems/Nav.vue
+++ src/components/menuItems/Nav.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>Nav</div>
+    <ul class="py-3">
+      <li
+      class="text-sm align-center
+       px-4 py-2 opacity-100 flex flex-row rounded mx-2 bg-activeColor cursor-pointer gap-x-4"
+      >
+         <img src="@/assets/home.svg" alt="home"/> Ana sayfa
+      </li>
+      <li class="px-4 py-2 opacity-70 cursor-pointer flex flex-row hover:rounded
+        hover:opacity-100 gap-x-4 mx-2">
+          <img src="@/assets/search.svg" alt="search"/>
+          Ara
+      </li>
+      <li class="px-4 py-2 opacity-70 cursor-pointer flex flex-row hover:rounded
+        hover:opacity-100 gap-x-4 mx-2">
+          <img src="@/assets/collection.svg" alt="collection"/>
+          Kitaplık
+      </li>
+    </ul>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
-
-<style>
-
-</style>
