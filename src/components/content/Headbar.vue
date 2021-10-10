d925c7317e06a59050eb55ed16f2b9ac9be8fca2
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-10 14:16:11 +0300

    Create headbar component

---
+++ src/components/content/Headbar.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="grid grid-cols-4 gap-6">
+        <Item v-for="index in 8" :key="index"/>
+    </div>
+</template>
+
+<script>
+import Item from '@/components/content/headbar/Item.vue';
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
