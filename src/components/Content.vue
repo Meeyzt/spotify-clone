64d751ef6c912988ca8fe65a53ee2e66b5b1cc3c
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-10 14:16:15 +0300

    Update Content.vue

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,15 +1,23 @@
 <template>
   <main class="flex-auto bg-contentColor">
     <Navbar/>
+    <div class="p-8 flex flex-col items-start">
+      <Head/>
+      <Headbar/>
+    </div>
   </main>
 </template>
 
 <script>
 import Navbar from '@/components/content/Navbar.vue';
+import Head from '@/components/content/Head.vue';
+import Headbar from '@/components/content/Headbar.vue';
 
 export default {
   components: {
     Navbar,
+    Head,
+    Headbar,
   },
 };
 </script>
