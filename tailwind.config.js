8602c9bf866c8f2a547f402c1458795e333128ea
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-10 15:40:55 +0300

    Set playbutton

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,14 @@ module.exports = {
         minimalColor: '#1b1718',
         hoverHeadbar: '#424040',
         headbarColor: '#423f3fa6',
+        spotifyGreen: '#1db954',
       },
       fontSize: {
         's': '.800rem',
         'header': ['1.80rem','36px']
+      },
+      spacing: {
+        13: '3.25rem',
       },
     },
   },
