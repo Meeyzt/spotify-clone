8062b204511f4163c92d54aa0ec6b3ed560ddcac
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-10 23:02:40 +0300

    Set content/Item hover

--- src/components/content/headbar/Item.vue
+++ src/components/content/headbar/Item.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="cursor-pointer w-72 bg-headbarColor grid grid-cols-3 items-center
+  <div class="group cursor-pointer w-72 bg-headbarColor grid grid-cols-3 items-center
   rounded-md hover:bg-hoverHeadbar justify-start">
         <div class="w-20 h-20 bg-pink-300 rounded-l-md shadow-2xl headerImg bg-center bg-cover"/>
         <div class="text-white text-base font-semibold">Rock mix</div>
-        <div class="ml-10 bg-spotifyGreen w-10 h-10 rounded-full flex items-center justify-center
-        ">
+        <div class="transition ml-10 bg-spotifyGreen w-10 h-10 rounded-full flex items-center
+        justify-center invisible bg-opacity-0 group-hover:bg-opacity-100 group-hover:visible">
+        <!-- grouphover -->
             <img src="@/assets/play.svg" alt="play"/>
         </div>
   </div>
