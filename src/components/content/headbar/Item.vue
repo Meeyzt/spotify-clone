8e599b70e1a996d085a4de4a04ec8adb6cb7c0f0
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-10-10 14:14:24 +0300

    Create headbarItem component

---
+++ src/components/content/headbar/Item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cursor-pointer w-72 bg-headbarColor flex flex-rows items-center
+         rounded-md hover:bg-hoverHeadbar">
+        <div class="w-20 h-20 bg-pink-300 rounded-l-md shadow-2xl headerImg bg-center bg-cover"/>
+        <div class="pl-4 text-white text-base font-semibold">Rock mix</div>
+        <div>
+
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
